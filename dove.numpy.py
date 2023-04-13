6fd9ea555e93f3d4e457f8c386f1f140dfc418b7
Madeline Estey <madeline.f.estey@gmail.com>
2023-04-12 23:14:01 -0400

    Making code work with Matrix class

--- dove.numpy.py
+++ dove.numpy.py
@@ -21,37 +21,36 @@ class Matrix():
         else: #case where external data is used... I think
              print("def ${} [1:{}] [1:{}]\n\tdataset {}\nend ${}".format(self.id, self.row, self.col, self.name, self.id))
 
-    def newMatrix(self, operand, operation):
+    def modifyMatrix(self, operand, operation):
         tmp = Matrix(self.row, self.col, None, operation)
         print("${} ${}\nend ${}".format(self.id, operand, tmp.id)) 
         return tmp
 
     def zeros(shape): #shape is int or tuple of ints
         if isinstance(shape, int): #1D array
-            file.write("def ${} [1:1] [1:{}]".format(opNum, shape))
+            new = Matrix(1, shape, None, None) #file.write("def ${} [1:1] [1:{}]".format(opNum, shape))
         else: #2D
-            file.write("def ${} [1:{}] [1:{}]".format(opNum, shape[1], shape[2])) 
-        file.write("== #0")
-        file.write("end ${}".format(opNum))
+            new = Matrix(shape[1], shape[2], None, None) file.write("def ${} [1:{}] [1:{}]".format(opNum, shape[1], shape[2])) 
+        file.write("\n\t== #0")
+        file.write("\nend ${}".format(opNum))
         opNum += 1
 
-
     def dot(item1, item2): #add cases for 1D vs 2D
+        #fix
         file.write("def ${} [1:{}] [1:{}]".format(opNum, shape[1], shape[2]))
         file.write("\t== ${} * ${}".format(opNum))
         file.write("end ${}".format(opNum))
         opNum += 1
 
-
     def sum(arr): # elements to sum, takes in array???
-        file.write("sum ${} ${}".format(opNum))
+        file.write("sum ${}".format(arr.id))
         file.write("set ${}".format(opNum))
         opNum += 1
 
     def exp(values):
-        file.write("def ${} [1:{}] [1:{}]".format(opNum, values.rows, values.columns))
-        file.write("\t exp ${}".format(values.id))
-        file.write("end ${}".format(opNum))
+        new = Matrix(values.rows, values.columns, none, none)
+        #new.modifyMatrix(, self, "^") <--- how to represent e???
+        #file.write("\t exp ${}".format(values.id))
         opNum += 1
 
     def array(data):
