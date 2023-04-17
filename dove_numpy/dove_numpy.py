14b782d1098c94dd20309bf7218a647036ba0def
Madeline Estey <madeline.f.estey@gmail.com>
2023-04-16 23:36:44 -0400

    Logistics regression functions almost done

--- dove_numpy/dove_numpy.py
+++ dove_numpy/dove_numpy.py
@@ -17,7 +17,7 @@ class Matrix():
         self.operation = operation
         opNum += 1
         if self.name == None: #matrix will be modified as specified later on in the user's program
-            print("def ${} [1:{}] [1:{}]\n\t{} ".format(opNum, self.row, self.col, self.operation), end = ' ')
+            print("def ${} [1:{}] [1:{}]\n\t{} ".format(self.iD, self.row, self.col, self.operation), end = ' ')
         else: #case where external data is used... I think
              print("def ${} [1:{}] [1:{}]\n\tdataset {}\nend ${}".format(self.iD, self.row, self.col, self.name, self.iD))
 
@@ -27,48 +27,27 @@ class Matrix():
         return tmp
 
 def zeros(shape): #shape is int or tuple of ints
-    global opNum
     if isinstance(shape, int): #1D array
-        m = Matrix(1, shape, None, None) #file.write("def ${} [1:1] [1:{}]".format(opNum, shape))
+        m = Matrix(1, shape, None, "==") #file.write("def ${} [1:1] [1:{}]".format(opNum, shape))
     else: #2D
-        m = Matrix(shape[1], shape[2], None, None) #file.write("def ${} [1:{}] [1:{}]".format(opNum, shape[1], shape[2])) 
-    print("\n\t== #0") #file.write
-    print("\nend ${}".format(opNum))
-    opNum += 1
+        m = Matrix(shape[0], shape[1], None, "==") #file.write("def ${} [1:{}] [1:{}]".format(opNum, shape[1], shape[2])) 
+    print("#0\nend ${}".format(opNum)) #file.write
 
 def dot(item1, item2): #add cases for 1D vs 2D
     m = Matrix.modifyMatrix(item1, item2, "*")
     return m
 
-def dot2(item1, item2):
-    Matrix.modifyMatrix(item1, item2, "*")
-
-def sum(arr): # elements to sum, takes in array???
-    global opNum
+def sum(arr): #elements to sum, takes in array???
     print("sum ${}".format(arr.iD))
     print("set ${}".format(opNum))
-    opNum += 1
 
-#def exp(values):
-    #new = Matrix(values.rows, values.columns, none, none)
-    #new.modifyMatrix(, self, "^") <--- how to represent e???
-    #file.write("\t exp ${}".format(values.id))
-    #opNum += 1
+def exp(values): #math.exp function does this... can represent e
+    m = Matrix.modifyMatrix(, values, none, "^")
 
-#def array(data):
-    #new = Matrix()
-    #file.write("\t dataset data ${}".format(values.id)) #??????????
-    #file.write("end ${}".format(opNum))
-    #opNum += 1
+def array(data): #can accept like any object... add options later
+    m = Matrix(data.row, data.col)
 
-#test code
-#test1 = Matrix(2, 2, "test1")
-#test2 = Matrix(2, 2, "test2")
 
-#dotproduct = dot(test1, test2)
-#zeromatrix = zeros((2, 1))
-#sum = sum([2, 1], [3, 4])
-#dotproduct2 = dot2(test1, test2) 
     
     
     
